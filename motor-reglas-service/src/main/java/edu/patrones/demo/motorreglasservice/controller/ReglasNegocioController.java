71f273f6abce1117eb9b2b865fa0f6c0635cf317
sabrosol <84605320+sabrosol@users.noreply.github.com>
2022-09-25 15:35:54 -0500

    Ajuste segunda oferta y prueba automatizada

--- motor-reglas-service/src/main/java/edu/patrones/demo/motorreglasservice/controller/ReglasNegocioController.java
+++ motor-reglas-service/src/main/java/edu/patrones/demo/motorreglasservice/controller/ReglasNegocioController.java
@@ -15,6 +15,15 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class ReglasNegocioController {
     private final KieContainer kieContainer;
+    private Double tasa;
+    private Double tasames;
+    private int plazot;
+    private Double valorcuoptames;
+    private Double valorcuotaredondeada;
+    private Double tasaefecmes;
+    private Double totalingresmengastos;
+    private Double factorajuste;
+    private Double totalingresos;
 
     public ReglasNegocioController(KieContainer kieContainer) {
         this.kieContainer = kieContainer;
@@ -26,48 +35,75 @@ public class ReglasNegocioController {
         log.warn("Plazo {}", orderRequest.getPlazo());
         log.warn("Valor Solicitado {}", orderRequest.getValorSolicitado());
         /*INICIO CALCULO CUOTA MES*/
-        Double tasa = 22.75/100;
-        //log.warn("tasa {}", tasa);
-        Double tasames = (1 +(tasa / 12));
-        //log.warn("tasames {}", tasames);
-        int plazot = (-(orderRequest.getPlazo() / 12) * 12);
-        //log.warn("plazot {}", plazot);
-        double valorcuoptames = (orderRequest.getValorSolicitado()*(tasa/12))/(1-Math.pow(tasames,plazot));
-        //log.warn("valorcuoptames {}", valorcuoptames);
-        double valorcuotaredondeada = Math.round(valorcuoptames*100.0)/100.0;
-        double tasaefecmes = tasames;
-        //log.warn("roundDbl {}", roundDbl);
-        log.warn("TASA MES {}", tasames);
-        log.warn("PLAZO {}", plazot);
-        log.warn("CUOTA CALCULADA {} - VALOR REDONDEADO {}: ", valorcuoptames, valorcuotaredondeada);
-
+        calcularCuota(orderRequest.getValorSolicitado(),orderRequest.getPlazo());
+        /*FIN CALCULO CUOTA MES*/
         /*Se validan el total de ingresos - gastos y factor de ajuste*/
-        log.warn("SalarioAportes {} - Gastos {} ", orderRequest.getSalarioAportes(), orderRequest.getGastos());
-        double totalingresmengastos = orderRequest.getSalarioAportes() - orderRequest.getGastos();
-        double factorajuste = (totalingresmengastos * 40 / 100);
-        log.warn("factorajuste {} - totalingresmengastos {} ", factorajuste, totalingresmengastos);
-        double totalingresos = totalingresmengastos - factorajuste;
-        log.warn("totalingresos {} ", totalingresos);
+        calculaTotalIngresos(orderRequest.getSalarioAportes(), orderRequest.getGastos());
 
         MotorReglaResponseDto respuesta = new  MotorReglaResponseDto();
         KieSession kieSession = kieContainer.newKieSession();
         kieSession.insert(orderRequest);
         kieSession.fireAllRules();
         kieSession.dispose();
-
+        log.warn("1. Valor Solicitado {} - valorcuotaredondeada {}", totalingresos, valorcuotaredondeada);
+        respuesta.setValorAprobado(orderRequest.getValorAprobado());
         if (valorcuotaredondeada > totalingresos){
             orderRequest.setCodeRespuesta(1699);
-            orderRequest.setMensajeE("Valor de la cuota > A Ingresos");
             respuesta.setValorAprobado(0);
+            orderRequest.setMensajeE("Valor de la cuota > A Ingresos");
         }
-        respuesta.setValorAprobado(orderRequest.getValorAprobado());
+        log.warn("2. Valor Solicitado {} - valorcuotaredondeada {} - CodError {} ", totalingresos, valorcuotaredondeada, orderRequest.getCodeRespuesta());
+        if (orderRequest.getCodeRespuesta() == 1699 && (totalingresos >= 400000 && totalingresos <= 1500000)){
+            calculaTotalIngresos(1500000.0, 0.0);
+            orderRequest.setCodeRespuesta(0);
+            respuesta.setValorAprobado(1500000);
+            orderRequest.setMensajeE("El valor sugerido nuevo es: " + respuesta.getValorAprobado());
+        }
+        //log.warn("3. Valor Solicitado {} - valorcuotaredondeada{} - tasaefecmes {}: " + orderRequest.getValorAprobado(), valorcuotaredondeada, tasaefecmes);
         respuesta.setValorCuota(valorcuotaredondeada);
         respuesta.setTasaCalculada(tasaefecmes);
-        /*FIN CALCULO CUOTA MES*/
+
         respuesta.setMensajeS(orderRequest.getMensajeE());
         respuesta.setCodeRespuesta(orderRequest.getCodeRespuesta());
         respuesta.setNumeroSolicitud(orderRequest.getNumeroSolicitud());
         log.warn("Resultado {} ", respuesta.getMensajeS());
         return respuesta;
+    }
+
+    public Double calcularCuota(Double valorSolicitado, int plazosolicitado){
+        try {
+            tasa = 22.75 / 100;
+            //log.warn("tasa {}", tasa);
+            tasames = (1 + (tasa / 12));
+            //log.warn("tasames {}", tasames);
+            plazot = (-(plazosolicitado / 12) * 12);
+            //log.warn("plazot {}", plazot);
+            valorcuoptames = (valorSolicitado * (tasa / 12)) / (1 - Math.pow(tasames, plazot));
+            //log.warn("valorcuoptames {}", valorcuoptames);
+            valorcuotaredondeada = Math.round(valorcuoptames * 100.0) / 100.0;
+            tasaefecmes = tasames;
+            //log.warn("roundDbl {}", roundDbl);
+            log.warn("TASA MES {} - PLAZO {}", tasames, plazot);
+            log.warn("CUOTA CALCULADA {} - VALOR CUOTA REDONDEADO {}: ", valorcuoptames, valorcuotaredondeada);
+
+        }catch (Exception e){
+            log.warn("Error en calculo de cuota");
+        }
+        return valorcuotaredondeada;
+    }
+
+    public Double calculaTotalIngresos(Double ingresos, Double Gastos){
+        try {
+            log.warn("SalarioAportes {} - Gastos {} ", ingresos, Gastos);
+            totalingresmengastos = ingresos - Gastos;
+            factorajuste = (totalingresmengastos * 40 / 100);
+            log.warn("factorajuste {} - totalingresmengastos {} ", factorajuste, totalingresmengastos);
+            totalingresos = totalingresmengastos - factorajuste;
+            log.warn("totalingresos {} ", totalingresos);
+
+        }catch (Exception e){
+            log.warn("Error en calcular el total de ingresos ");
+        }
+        return totalingresos;
     }
 }
